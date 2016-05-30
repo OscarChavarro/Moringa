ff80cfb9cb4b2c283912b9c610d2565fc8398192
Alexis <alexis.fereira@gmail.com>
2016-05-29 19:38:04 -0430

    menu ajustado

---
+++ client/templates/global/layout.js
@@ -0,0 +1,24 @@
+Template.layout.helpers({
+    "mifuncion" : function() {
+      console.log("Hola. Este es un código JavaScript metido dentro de un HELPER");
+      var componente;
+      componente = $("centralArea");
+      console.log(componente);
+
+    }
+
+});
+
+Template.layout.onRendered(function(){
+    $(window).scroll(function(){
+        var vscroll= $(this).scrollTop();
+        if(vscroll>$(window).height()){
+            $('.cabecera-fija').addClass('mas-fino');
+            $('.logo img').addClass('mas-fino-img');
+        }else{
+            $('.cabecera-fija').removeClass('mas-fino');
+            $('.logo img').removeClass('mas-fino-img');
+        }
+    });
+    console.log("Esto se ejecuta tan pronto se muestra la plantilla");
+});
